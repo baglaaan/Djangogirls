566b549d425eafe27ddbc38d6fe9339770a2133b
baglaaan <160104017@stu.sdu.edu.kz>
2018-12-15 17:56:09 +0600

    Django-REST

---
+++ blog/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from blog.models import Post
+
+
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta: 
+        model = Post
+        fields = ('title', 'text', 'published_date')
+
+
